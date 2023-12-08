fdb985e41c5e47d21e227a3d008dd4267fee68aa
Ha__sh <anelegistfrom98@yahoo.com>
2023-12-08 15:03:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# SHAi
+ 
+Empowering Agriculture Through Knowledge
+
+## Introduction:
+SHAi, which stands for Sustainable Hydroponic AI, is a groundbreaking project aimed at revolutionizing hydroponic farming practices. Focused on sustainability, this domain-specific Large Language Model (LLM) is designed to cater to the diverse needs of hydroponic enthusiasts while aligning with eco-friendly agricultural methods.
+
+## Key Features:
+
+1. Comprehensive Guidance: SHAi provides comprehensive guidance for hydroponic farming, covering everything from basic principles to advanced industrial applications.
+2. Sustainability Integration: Aligned with sustainable agricultural methods, SHAi promotes eco-conscious practices, contributing to a more environmentally friendly approach in hydroponics.
+3. Domain-Specific Expertise: Tailored specifically for hydroponics, SHAi ensures that users receive specialized and relevant information, fostering success in their farming endeavors.
+4. User-Friendly Interface: The SHAi interface is designed for accessibility, offering an intuitive platform for users at all skill levels, from beginners to experts.
+
+## Hydroponics - A modern agricultural method
+Hydroponics is a method of growing plants in water instead of soil. This can be done indoors or outdoors, and it can be used to grow a variety of different plants, including fruits, vegetables, and herbs.
+
+Hydroponics is a sustainable method of growing plants that can help to improve food security and reduce the environmental impact of agriculture.
+
+One of the main benefits of hydroponics is that it can be more efficient than traditional soil-based farming. This is because hydroponic systems can be more precisely controlled, which allows for better use of water and nutrients. In addition, hydroponic systems can be used to grow plants in areas where soil is not suitable for farming, such as in urban areas or in arid climates.
+
+Hydroponics can also be used to grow plants year-round, which is not possible with traditional soil-based farming. This is because hydroponic systems can be controlled to provide the ideal growing conditions for plants, regardless of the time of year.
+
+Another benefit of hydroponics is that it can reduce the amount of water and nutrients used to grow plants. This is because hydroponic systems recycle water and nutrients, which reduces the amount of waste produced. In addition, hydroponic systems can be used to grow plants with less water and nutrients than traditional soil-based farming methods.
+
+Finally, hydroponics can help to reduce the amount of pollution produced by traditional farming methods. This is because hydroponic systems do not require the use of pesticides or herbicides, which can pollute the environment. In addition, hydroponic systems can be located close to urban areas, which reduces the need to transport food long distances.
+
+Overall, hydroponics is a sustainable method of growing plants that can help to improve food security and reduce the environmental impact of agriculture.
+
+## Problem Statement:
+The absence of a comprehensive and accessible guide for hydroponic farming, spanning from home-based enthusiasts to industrial-scale practitioners, poses a significant barrier to widespread adoption. The risk of biased information and unsustainable agricultural practices obtained through internet searches, along with potential errors and human-induced hallucinations leading to untested theories, further complicates the reliable dissemination of knowledge in hydroponic farming. Bridging this gap is essential to encourage more people to embrace this innovative approach to agriculture.
+
+## Proposed Solution:
+In the dynamic landscape of hydroponic farming, accessibility to reliable information is crucial, spanning from home-based enthusiasts to industrial-grade practitioners. To address this, our project utilizes cutting-edge technologies, integrating Zilliz for storing vector embeddings derived from Google PALM Embeddings. This innovative approach harnesses web-scraped data stored in Github Markdown files to create a robust vector knowledge base.
+
+## Methodology:
+The SHAi project leverages the LlamaIndex framework to streamline the Retrieval Augmented Generation (RAG) process, enhancing the querying of the Zilliz vector database. With the assistance of LlamaIndex, context and prompts are efficiently extracted, enriching the information passed to Vertex AI's PaLM (Parameterized Language Model) for generating more prominent and relevant responses. This synergy between LlamaIndex and RAG ensures that the Language Model eliminates LLM hallucinations and accommodates a diverse audience, offering tailored guidance across proficiency levels in both home-based and industrial-grade hydroponic farming. The entire system is seamlessly integrated into a user-friendly Streamlit app, further enhancing accessibility and user interaction.
+
+## Evaluation:
+To gauge the effectiveness and relevance of our system, we employ Truera, an AI Observability platform. This platform serves as a comprehensive tool for LLM evaluation, ensuring that the responses generated by PALM align with the user's queries and contribute to the overall knowledge enhancement in hydroponic farming.
+
+## Conclusion:
+SHAi emerges as a powerful ally for those venturing into hydroponic farming, promoting sustainability and providing a wealth of knowledge in a user-friendly manner. As we advance towards a more sustainable future, SHAi stands at the forefront, guiding individuals on their hydroponic journey with expertise and eco-consciousness.
---
+++ main.py
@@ -0,0 +1,169 @@
+import streamlit as st
+from openai import OpenAI
+from glob import glob
+from llama_index import download_loader, VectorStoreIndex, ServiceContext
+from llama_index.vector_stores import MilvusVectorStore
+from llama_index.embeddings import HuggingFaceEmbedding
+from llama_index import ServiceContext
+from llama_index.llms import PaLM
+from llama_index.embeddings import GooglePaLMEmbedding
+from llama_index.memory import ChatMemoryBuffer
+import numpy as np
+from trulens_eval import TruLlama, Tru, Query, Feedback, feedback
+import google.generativeai as palm
+import os
+import subprocess
+import sys
+
+def clone_or_update_git_repo(repo_url, local_dir):
+    # Check if the directory exists
+    if os.path.exists(local_dir):
+        print(f"Directory '{local_dir}' already exists. Updating the repository...")
+        # If the directory exists, perform a git pull to update the repository
+        subprocess.run(['git', 'pull'], cwd=local_dir, check=True)
+    else:
+        print(f"Directory '{local_dir}' does not exist. Cloning the repository...")
+        # If the directory does not exist, clone the repository
+        subprocess.run(['git', 'clone', repo_url, local_dir], check=True)
+
+# Example usage
+repository_url = 'https://github.com/being-invincible/hydroponics-grow-guide-dataset'
+local_directory = './hydroponics-grow-guide-dataset'
+
+clone_or_update_git_repo(repository_url, local_directory)
+
+# 1. Set up the name of the collection to be created.
+COLLECTION_NAME = 'hydroponics_knowledge_base'
+
+# 2. Set up the dimension of the embeddings.
+DIMENSION = 1536
+
+# 3. Set the inference parameters
+BATCH_SIZE = 128
+TOP_K = 3
+
+# 4. Set up the connection parameters for your Zilliz Cloud cluster.
+URI = st.secrets['CLUSTER_ENDPOINT']
+
+TOKEN = st.secrets['API_TOKEN']
+
+# Palm API
+palm_api_key = st.secrets['PALM_API_KEY']
+
+palm.configure(api_key=palm_api_key)
+
+models = [
+    m
+    for m in palm.list_models()
+    if "generateText" in m.supported_generation_methods
+]
+model = models[0].name
+print(model)
+
+llm = PaLM(api_key=palm_api_key)
+
+# .streamlit/secrets.toml
+# # OpenAI API key
+os.environ["OPENAI_API_KEY"] = st.secrets["OPEN_API_KEY"]
+
+# Load the markdown reader from the hub
+MarkdownReader = download_loader("MarkdownReader")
+markdownreader = MarkdownReader()
+
+memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
+
+# Query Engine
+# query_engine = index.as_query_engine()
+
+# # Truera Wrapper
+# l = TruLlama(query_engine)
+
+st.title("SHAi")
+st.text("Sustainable Hydroponic AI")
+
+# client = TruLlama(query_engine)
+
+if "openai_model" not in st.session_state:
+    # st.session_state["openai_model"] = "gpt-3.5-turbo"
+    pass
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about Hydroponics & I will try to answer it in a sustainable way possible!"}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the vectors from Zilliz – hang tight! This should take 30 - 50 seconds"):
+        # Grab all markdown files and convert them using the reader
+        docs = []
+        for file in glob("./hydroponics-grow-guide-dataset/*.md", recursive=True):
+            docs.extend(markdownreader.load_data(file=file))
+        
+        # Push all markdown files into Zilliz Cloud
+        vector_store = MilvusVectorStore(
+            uri=URI, 
+            token=TOKEN, 
+            collection_name=COLLECTION_NAME, 
+            similarity_metric="L2",
+            dim=DIMENSION,
+        )
+        
+        llm=PaLM(api_key=palm_api_key)
+        
+        # Service Context - PALM - Vertex AI
+        embed_model = GooglePaLMEmbedding("models/embedding-gecko-001", api_key=palm_api_key)
+        service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model)
+
+        index = VectorStoreIndex.from_documents(
+            documents=docs, 
+            service_context=service_context,
+            show_progress=True,
+        )
+
+        return index
+
+index = load_data()
+
+# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+chat_engine = index.as_chat_engine(chat_mode="context",
+    memory=memory, verbose=True)
+
+# Initialize Huggingface-based feedback function collection class:
+hugs = feedback.Huggingface()
+openai = feedback.OpenAI()
+# Define a language match feedback function using HuggingFace.
+f_lang_match = Feedback(hugs.language_match).on_input_output()
+# By default this will check language match on the main app input and main app
+# output.
+
+# Question/answer relevance between overall question and answer.
+f_qa_relevance = Feedback(openai.relevance).on_input_output()
+
+# Question/statement relevance between question and each context chunk.
+f_qs_relevance = Feedback(openai.qs_relevance).on_input().on(
+    TruLlama.select_source_nodes().node.text
+).aggregate(np.min)
+
+feedbacks = [f_lang_match, f_qa_relevance, f_qs_relevance]
+
+chat_engine = TruLlama(chat_engine, feedbacks=feedbacks, app_id="SHAi App")
+
+tru = Tru()
+tru.run_dashboard()
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
